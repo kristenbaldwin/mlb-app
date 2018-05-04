1577c863dcae06313b7ddb8c72090af15345c568
Kristen Baldwin <kristenbaldwin@Kristens-MBP.localdomain>
2018-05-04 15:44:57 -0500

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './App';
+import Store from './store';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+import registerServiceWorker from './registerServiceWorker';
+
+const StoreInstance = Store();
+
+ReactDOM.render(
+    <Provider store={StoreInstance}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+registerServiceWorker();
