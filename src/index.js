1a4288b7e03f9128815e0315b9eb8bd6884b0fe8
Kristen Baldwin <kristenbaldwin@Kristens-MBP.localdomain>
2018-05-11 10:28:55 -0500

    Fixed Store error with too many arguments

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import './styles/index.css';
 import App from './App';
 import Store from './store';
 import { Provider } from 'react-redux';
-import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const StoreInstance = Store(rootReducer);
+const StoreInstance = Store();
 
 ReactDOM.render(
     <Provider store={StoreInstance}>
